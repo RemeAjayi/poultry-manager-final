aeb43debcc81ab8375a0486355d46a687f201555
Nicholas Kajoh <kajohterna@gmail.com>
2018-03-08 22:21:36 +0100

    setup project as an mvc

--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
 const path = require('path');
 const express = require('express');
-const publicPath = path.join(__dirname, '../public');
 
+// create Express app
 var app = express();
-app.use(express.static(publicPath));
-console.log(publicPath);
-app.listen(3000, () => {
+
+// setup views folder
+app.set('views', path.join(__dirname, '../views'));
+// setup view engine
+app.set('view engine', 'ejs');
+
+// setup static files directory
+app.use(express.static(path.join(__dirname, '../public')));
+
+// define app routes
+app.use('/', require('../controllers/dashboard'));
+
+// start Node server
+app.listen(process.env.PORT || 3000, () => {
    console.log("Server is up on port 3000"); 
 });
